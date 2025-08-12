5dfb0c160d01d27a6015aa90c9a375c7cbf58176
Eduardo C F Neto <eduardo@verion.ind.br>
2025-08-12 14:46:10 -0300

    Initial commit.

---
+++ EdwardNoteBoxes/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EdwardNoteBoxes")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EdwardNoteBoxes.Pages")]
---
+++ EdwardNoteBoxes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EdwardNoteBoxes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
